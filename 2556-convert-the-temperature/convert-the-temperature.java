c5e080f78ee7bd0b69674209ce8f24e1d18cf163
rakshit89921 <rn.girase2105@gmail.com>
2026-07-29 00:00:08 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (9.40%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.java
+++ 2556-convert-the-temperature/convert-the-temperature.java
@@ -1,14 +1,11 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
 
-        double toKelvin = celsius + 273.15;
-        double toFaren = celsius *1.80 + 32.00;
+        double[] ans = new double[2];
 
-        return new double[] {toKelvin,toFaren};
+        ans[0] = celsius + 273.15;
+        ans[1] = celsius * 1.80 + 32.00;
 
-        
-
-
-        
+        return ans;
     }
 }
